742606fb5afba7ae4f9358952fb866badb247410
johnnyferreiradevweb <johnnyferreiradevweb@gmail.com>
2019-12-11 21:31:53 -0300

    Resolução do exercício 02

---
+++ lista_exercicio_3_js/exercicio02/js/index.js
@@ -0,0 +1,13 @@
+const numeros = [];
+let maior = -Infinity;
+let qtd = 0;
+
+while(qtd < 5) {
+    var numero = parseInt(prompt('Digite um numero'));
+    if (numero > maior) {
+        maior = numero;
+    }
+    qtd++
+}
+
+alert(`O maior numero digitado foi o ${maior}`);
